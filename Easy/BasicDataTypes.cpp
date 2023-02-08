b7ccfdf319a4218632d29b81e53661dd0f2783e7
Gong Bongjin <113762622+GongBongjin@users.noreply.github.com>
2023-02-08 11:17:19 +0900

    Update: 230208
    commit 3 solutions.

---
+++ Easy/BasicDataTypes.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main()
+{
+    // Complete the code.
+    int a;
+    long b;
+    char c;
+    float d;
+    double e;
+
+    cin >> a >> b >> c >> d >> e;
+
+    cout << a << endl;
+
+    cout << b << endl;
+
+    cout << c << endl;
+
+    cout << fixed;
+    cout << d << endl;
+    cout.precision(10);
+
+    cout << fixed;
+    cout << e << endl;
+    cout.precision(20);
+
+    return 0;
+}
---
+++ Easy/InputandOutput.cpp
@@ -0,0 +1,21 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main()
+{
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    int a = 0;
+    int b = 0;
+    int c = 0;
+
+    cin >> a >> b >> c;
+
+    cout << a + b + c;
+
+    return 0;
+}
